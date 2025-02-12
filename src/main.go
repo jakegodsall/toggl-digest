118404e54eb2206e17af612be39f9571b72fdbcc
Jake Godsall <jake@cloudconstruct.co.uk>
2025-02-12 11:00:14 +0000

    added basic API handling

--- src/main.go
+++ src/main.go
@@ -1,27 +1,70 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var greeting string
-	sourceIP := request.RequestContext.Identity.SourceIP
+	email := os.Getenv("TOGGL_EMAIL")
+	password := os.Getenv("TOGGL_PASSWORD")
 
-	if sourceIP == "" {
-		greeting = "Hello, world!\n"
-	} else {
-		greeting = fmt.Sprintf("Hello, %s!\n", sourceIP)
+	auth := email + ":" + password
+
+	fmt.Println(auth)
+	authEncoded := base64.StdEncoding.EncodeToString([]byte(auth))
+	authHeader := "Basic " + authEncoded
+
+	
+	// Define the GET request
+	apiURL := "https://api.track.toggl.com/api/v9/me"
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Error creating request: %s", err.Error()),
+			StatusCode: 500,
+			}, nil
+		}
+		
+		req.Header.Add("Authorization", authHeader)
+		
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Error making request: %s", err.Error()),
+			StatusCode: 500,
+		}, nil
 	}
 
-	fmt.Printf("THIS IS A TEST!")
+	if resp.StatusCode == http.StatusUnauthorized {
+		return events.APIGatewayProxyResponse{
+			Body:       "Unauthorised: Invalid Toggl credentials",
+			StatusCode: 401,
+		}, nil
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Error reading response: %s", err.Error()),
+			StatusCode: 500,
+		}, nil
+	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       greeting,
-		StatusCode: 200,
+		Body:       string(body),
+		StatusCode: resp.StatusCode,
 	}, nil
 }
 
